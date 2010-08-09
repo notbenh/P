4671507cd616e4e3ada861634b567644a4dfe7ac
ben hengst <notbenh@cpan.org>
2010-08-09 09:24:45 -0700

    poking at attempts to rebuild the type idea

--- lib/P.pm
+++ lib/P.pm
@@ -1,12 +1,54 @@
 package P;
 use Dancer ':syntax';
+use Util::Log;
 
-# ABSTRACT: test app
+sub manage_type {
+   my $type = 'default'; # if cascade to allow for precidance
 
-our $VERSION = '0.1';
+   # .ext => type
+   if ( request->path =~ m/[.](.+)$/ ) {
+      $type = $1;
+      my $sans_ext = request->path;
+      $sans_ext =~ s/[.]$type$//;
+      request->path($sans_ext);
+   }
 
-get '/' => sub {
-    template 'index';
+   # ?type => type
+   if ( exists params->{type} ) {
+      my $p = request->{params};
+      $type = delete $p->{type};
+      delete request->{_query_params}->{type}; # remove it from here too
+   }
+
+   var type => $type;
 };
+
+
+sub display (@) {
+   my $code = shift;
+   my @data = map{ ref($_) eq 'CODE' ? &$_ : $_ } @_;
+#   sub{ DUMP {DISP => @data} };
+   return sub{ DUMP \@data }->(@data);
+}
+
+#---------------------------------------------------------------------------
+#  ROUTES
+#---------------------------------------------------------------------------
+
+before \&manage_type;
+
+any qr{.*} => display \&DD;
+
+
+sub DD {
+error 'DD';
+   { PARAM => {params},
+     VARS  => vars,
+     SPLAT => [splat],
+     URI   => request->path,
+   }
+};
+
+
 
 true;
