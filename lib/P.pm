68828af087f1130ea65bfdb7bcfc2229e3fc7535
ben hengst <notbenh@cpan.org>
2010-08-15 09:36:20 -0700

    working out the display stuff, pulling back in some old code to make that happen

--- lib/P.pm
+++ lib/P.pm
@@ -2,6 +2,16 @@ package P;
 use Dancer ':syntax';
 use Util::Log;
 
+
+sub manage_layout {
+   if( my $layout = delete params->{layout} ) { # no point in keeping it in the params stream, will only complicate things
+      my $template = Dancer::Template::Abstract->new->view( join '/', 'layouts', $layout );
+      var layout => ( -r $template) ? $layout : 'main';
+   } else {
+      var layout => 'main';
+   }
+};
+
 sub manage_type {
    my $type = 'default'; # if cascade to allow for precidance
 
@@ -20,33 +30,50 @@ sub manage_type {
       delete request->{_query_params}->{type}; # remove it from here too
    }
 
-   var type => $type;
+   if ( $type =~ m/(?:json|xml|yaml)/ ) {
+      set serializer => $type
+   }
+
+   var type => lc($type);
 };
 
 
 sub display (@) {
-   my $code = shift;
-   my @data = map{ ref($_) eq 'CODE' ? &$_ : $_ } @_;
-#   sub{ DUMP {DISP => @data} };
-   return sub{ DUMP \@data }->(@data);
+   if ( defined vars->{type} ) {
+      if ( vars->{type} eq 'data' ) {
+         return template data => {data => D @_};
+      }
+
+      my $template = join '/', grep{defined} vars->{template_dir}, vars->{type};
+      # switch to 'default' if we can't find that template
+      unless ( -r Dancer::Template::Abstract->new->view($template)) {
+         $template = join '/', grep{defined} vars->{template_dir}, 'default';
+      }
+
+      return scalar(@_) ? template $template => {data => @_} : template $template;
+   }
+   else {
+      return [@_]; # seralize
+   }
 }
 
 #---------------------------------------------------------------------------
 #  ROUTES
 #---------------------------------------------------------------------------
 
+before \&manage_layout;
 before \&manage_type;
 
-any qr{.*} => display \&DD;
-
+any qr{.*} => \&DD ;
 
 sub DD {
-error 'DD';
+DUMP {IN => \@_};
+   display( 
    { PARAM => {params},
      VARS  => vars,
      SPLAT => [splat],
      URI   => request->path,
-   }
+   });
 };
 
 
